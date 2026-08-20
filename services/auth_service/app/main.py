51c26196fcaea3edbedaa45b05a978bb0b637b94
mukeshsharma99 <mukesh10400@gmail.com>
2026-08-20 11:58:35 +0530

    push

--- services/auth_service/app/main.py
+++ services/auth_service/app/main.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI
+from services.auth_service.app.routes.health import router as health_router
 
 app = FastAPI()
 
-
-@app.get("/health")
-def health_check():
-    return {"status": "healthy"}
+app.include_router(health_router)
