1907b0e608d53f0a4e64495accdc66c67fed296e
mukeshsharma99 <mukesh10400@gmail.com>
2026-08-23 22:47:27 +0530

    push

--- services/auth_service/app/main.py
+++ services/auth_service/app/main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
-from services.auth_service.app.routes.health import health_router
+from services.auth_service.app.models.user import User
 
 app = FastAPI()
 
-app.include_router(health_router)
+
+@app.get("/health")
+def health_check():
+    return {"Name": "Status"}
+
+
+@app.post("/user")
+def create_user(user: User):
+    return user
