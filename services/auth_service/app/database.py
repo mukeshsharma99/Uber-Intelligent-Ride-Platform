71cbd548811a6a1ca8e45edca0be0a83c5f8eb75
mukeshsharma99 <mukesh10400@gmail.com>
2026-08-26 00:21:53 +0530

    Add PostgreSQL database connection

---
+++ services/auth_service/app/database.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+
+DATABASE_URL = "postgresql://uber_user:uber_password@localhost:5432/uber_db"
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit = False,
+    autoflush = False,
+    bind = engine
+    )
+
+Base = declarative_base()
