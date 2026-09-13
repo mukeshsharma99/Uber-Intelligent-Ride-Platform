779d75291ea3e8076c15c9b2fba1ab53258f3843
mukeshsharma99 <mukesh10400@gmail.com>
2026-09-13 21:56:12 +0530

    adding rider in auth based user registration

--- services/auth_service/app/schemas/user.py
+++ services/auth_service/app/schemas/user.py
@@ -1,12 +1,20 @@
+from typing import Literal
 from pydantic import BaseModel, EmailStr
 
 
 class UserCreate(BaseModel):
+
     username: str
+
     email: EmailStr
+
     password: str
+
+    role: Literal["RIDER", "DRIVER", "ADMIN"] = "RIDER"
 
 
 class UserLogin(BaseModel):
+
     username: str
-    password: str   
+
+    password: str
