90bb3982590da50a30aca57bcb3d4566e43d0edc
mukeshsharma99 <mukesh10400@gmail.com>
2026-09-14 23:22:43 +0530

    update main

--- services/auth_service/app/main.py
+++ services/auth_service/app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 
 from services.auth_service.app.database import Base, engine
+
 from services.auth_service.app.models.user import User
+from services.auth_service.app.models.rider import Rider
+
 from services.auth_service.app.routes.health import health_router
 from services.auth_service.app.routes.auth import auth_router
 
@@ -11,4 +14,5 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 app.include_router(health_router)
+
 app.include_router(auth_router)
