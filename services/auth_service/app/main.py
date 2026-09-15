ed1f7c1705cd9548471e74497f7e6e97b272f381
mukeshsharma99 <mukesh10400@gmail.com>
2026-09-15 23:29:48 +0530

    update rider profile progress

--- services/auth_service/app/main.py
+++ services/auth_service/app/main.py
@@ -3,10 +3,11 @@ from fastapi import FastAPI
 from services.auth_service.app.database import Base, engine
 
 from services.auth_service.app.models.user import User
-from services.auth_service.app.models.rider import Rider
+from services.auth_service.app.models.rider_model import Rider
 
 from services.auth_service.app.routes.health import health_router
 from services.auth_service.app.routes.auth import auth_router
+from services.auth_service.app.routes.rider_routes import rider_router
 
 
 Base.metadata.create_all(bind=engine)
@@ -14,5 +15,5 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 app.include_router(health_router)
-
 app.include_router(auth_router)
+app.include_router(rider_router)
